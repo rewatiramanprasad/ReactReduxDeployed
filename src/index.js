fc97f9f09af68eb6a3d59d4dc4c092c6f48d77fa
Raman Prasad <60893770+rewatiramanprasad@users.noreply.github.com>
2024-03-02 05:54:29 +0000

    Create slice

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { sendToVercelAnalytics } from './vitals';
-
-ReactDOM.render(
-  <React.StrictMode>
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "../src/store/store";
+import reportWebVitals from "./reportWebVitals";
+import { sendToVercelAnalytics } from "./vitals";
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
 );
 
 reportWebVitals(sendToVercelAnalytics);
